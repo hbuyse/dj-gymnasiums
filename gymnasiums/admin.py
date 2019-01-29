54ac9e8de226a63145c3ab66e05776c59447d943
Henri Buyse <henri.buyse@gmail.com>
2019-01-29 17:08:37 +0100

    Admin update import

--- gymnasiums/admin.py
+++ gymnasiums/admin.py
@@ -1,14 +1,14 @@
 """..."""
 
 # Django
-from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
 # Current django project
 from gymnasiums.models import Gymnasium
 
 
 @admin.register(Gymnasium)
-class GymnasiumAdmin(admin.ModelAdmin):
+class GymnasiumAdmin(ModelAdmin):
     """Gymnasium admin object."""
 
     list_display = ('name', 'city')
