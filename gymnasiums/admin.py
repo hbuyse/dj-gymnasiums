39f590dc703eec01c753ea54d7f4afd06f81a582
Henri Buyse <henri.buyse@gmail.com>
2019-01-29 22:53:21 +0100

    Fix flak7 erro

--- gymnasiums/admin.py
+++ gymnasiums/admin.py
@@ -1,13 +1,13 @@
 """..."""
 
 # Django
-from django.contrib.admin import ModelAdmin
+from django.contrib.admin import ModelAdmin, register
 
 # Current django project
 from gymnasiums.models import Gymnasium
 
 
-@admin.register(Gymnasium)
+@register(Gymnasium)
 class GymnasiumAdmin(ModelAdmin):
     """Gymnasium admin object."""
 
